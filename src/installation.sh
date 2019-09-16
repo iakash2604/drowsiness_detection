32be2c8bf3e0ea951cf3e2ec4d304cf703a37363
iakash2604 <iakash2702@gmail.com>
2019-09-17 01:12:19 +0530

    add installation.sh

---
+++ src/installation.sh
@@ -0,0 +1,11 @@
+pip install scipy;
+pip install imutils;
+pip install numpy; 
+pip install playsound; 
+
+sudo apt-get install build-essential cmake;
+sudo apt-get install libgtk-3-dev;
+sudo apt-get install libboost-all-dev;
+sudo apt-get install build-essential cmake;
+sudo apt-get install libgtk-3-dev;
+sudo apt-get install libboost-all-dev;
