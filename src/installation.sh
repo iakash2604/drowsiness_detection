f2be9d0f2deb75376deaa735787c48cdc697af2c
Akash Nimmagadda <iakash2702@gmail.com>
2019-09-30 12:27:01 +0530

    add dlib installation commands

--- src/installation.sh
+++ src/installation.sh
@@ -2,6 +2,8 @@ pip install scipy;
 pip install imutils;
 pip install numpy; 
 pip install playsound; 
+pip install numpy;
+pip install scikit-image;
 
 sudo apt-get install build-essential cmake;
 sudo apt-get install libgtk-3-dev;
@@ -9,3 +11,4 @@ sudo apt-get install libboost-all-dev;
 sudo apt-get install build-essential cmake;
 sudo apt-get install libgtk-3-dev;
 sudo apt-get install libboost-all-dev;
+pip install dlib;
